4f16cc916b26b4c9c77985d2504708f8d1df4791
Antonios Fiala <56882687+antoniosfiala@users.noreply.github.com>
2020-06-17 12:39:58 +0100

    Data and python work

--- Data/script_0_to_1.py
+++
@@ -1,6 +0,0 @@
-# %load script_0_to_1.py
-# This script allows one to load and correct raw files before saving them again.
-
-file_path_0_raw = ""
-file_path_1_backup = ""
-file_path_2_input = ""
---
+++ Data/script_filepaths.py
@@ -0,0 +1,5 @@
+# This script allows one to load and correct raw files before saving them again.
+file_path_0_raw       = "./0_raw/"
+file_path_1_backup    = "./1_raw_processed_backup/"
+file_path_2_input     = "./2_raw_processed_input/"
+file_path_3_generated = "./3_generated_inputs/"
